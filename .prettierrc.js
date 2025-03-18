4bc67a03ad73550565bf1dc90345c9183184af0d
Woojin Lee <nijoow1127@gmail.com>
2025-03-18 12:09:33 +0900

    prettier 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
